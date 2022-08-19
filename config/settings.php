323c0d5b42b958c4fb05c1682775fe2f1c542bd1
Arif hendika Rios <arifrios1st@gmail.com>
2022-08-20 05:21:40 +0700

    Add Web Settings

---
+++ config/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Rupat Pariwisata',
+    'email' => 'kafikanurdiana42@gmail.com',
+    'phone' => '+6283234352947',
+    'address' => 'Rupat Utara, Indonesia',
+    'fb_link' => '#',
+    'ig_link' => '#',
+    'yt_link' => '#',
+    'tw_link' => '#',
+];
